97bcb9ec9c0c8f0d1b56e65842a4a6ac1b51521e
Gaurav Mazra <gaurav.r.mazra@gmail.com>
2017-07-22 12:14:49 -0400

    Error is stringy to JSOn

--- fileOperations.js
+++ fileOperations.js
@@ -1,14 +1,14 @@
 var fs = require('fs');
 var buff = new Buffer(100000);
 
-fs.open('test_file.txt', 'r', (err, fd) => {
+fs.open('test_file1.txt', 'r', (err, fd) => {
   if (err) {
-    console.error("Got error while opening file: " + err.message);
+    console.error("Got error while opening file: " + JSON.stringify(err));
   }
   else {
     fs.read(fd, buff, 0, 100000, null, (err, length) => {
       if (err) {
-        console.error("Got error while reading file: " + err.message);
+        console.error("Got error while reading file: " + JSON.stringify(err));
       }
       else {
         console.log(buff.toString("utf8", 0, length));
